6dab06386cadb6e1245f4030146985291bf9bb1a
Kaushal <kaushal.shah@actonate.com>
2021-05-14 13:46:18 +0530

    feat: auto generate components

---
+++ config/templates/page/TemplateName.tsx
@@ -0,0 +1,12 @@
+import Layout from '@layouts/index';
+import ComponentName from '@components/TestDemo';
+
+const TemplateName = () => (
+  <Layout>
+    <div className='container mx-auto'>
+      <ComponentName />
+    </div>
+  </Layout>
+);
+
+export default TemplateName;
