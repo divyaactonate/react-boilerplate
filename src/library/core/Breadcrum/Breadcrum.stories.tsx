292cd12f11a86645b65fd3a03c5b34a1b3ba4e0f
Priya Singh <priya.singh@actonate.com>
2021-05-26 13:03:01 +0530

    Added breadcrum stories

--- src/library/core/Breadcrum/Breadcrum.stories.tsx
+++ src/library/core/Breadcrum/Breadcrum.stories.tsx
@@ -1,4 +1,66 @@
 import { storiesOf } from '@storybook/react';
 import { Breadcrum } from './index';
+import { Text } from '@library/core/Text';
+import { DEFAULT_THEME } from '@library/theme/default-theme';
 
-storiesOf('@beautify/core/Breadcrum', module).add('default', () => <Breadcrum />);
+const items = [
+  { title: 'Mantine', href: 'https://mantine.dev' },
+  { title: 'Mantine hooks', href: 'https://mantine.dev/hooks/getting-started' },
+  { title: 'use-id', href: 'https://mantine.dev/hooks/use-id' },
+].map((item, index) => (
+  <Text<'a'> variant='link' color={'blue'} component='a' href={item.href} key={index}>
+    {item.title}
+  </Text>
+));
+
+storiesOf('@beautify/core/Breadcrum', module)
+  .add('General usage', () => (
+    <div style={{ padding: 50 }}>
+      <Breadcrum>{items}</Breadcrum>
+    </div>
+  ))
+  .add('Custom separator', () => (
+    <div style={{ padding: 50 }}>
+      <Breadcrum style={{ marginTop: 20 }} separator='→'>
+        {items}
+      </Breadcrum>
+
+      <Breadcrum
+        style={{ marginTop: 20 }}
+        separator={
+          <svg
+            xmlns='http://www.w3.org/2000/svg'
+            className='h-4 w-4'
+            fill='none'
+            viewBox='0 0 24 24'
+            stroke='grey'
+          >
+            <path strokeLinecap='round' strokeLinejoin='round' strokeWidth={2} d='M9 5l7 7-7 7' />
+          </svg>
+        }
+      >
+        {items}
+      </Breadcrum>
+    </div>
+  ))
+  .add('Dark theme', () => (
+    <div style={{ background: DEFAULT_THEME.colors.dark[7], minHeight: '100vh', padding: 50 }}>
+      <Breadcrum themeOverride={{ colorScheme: 'dark' }}>
+        {[
+          { title: 'Mantine', href: 'https://mantine.dev' },
+          { title: 'Mantine hooks', href: 'https://mantine.dev/hooks/getting-started' },
+          { title: 'use-id', href: 'https://mantine.dev/hooks/use-id' },
+        ].map((item, index) => (
+          <Text<'a'>
+            variant='link'
+            component='a'
+            href={item.href}
+            key={index}
+            themeOverride={{ colorScheme: 'dark' }}
+          >
+            {item.title}
+          </Text>
+        ))}
+      </Breadcrum>
+    </div>
+  ));
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,19 @@
 import { useState } from 'react';
 import Layout from '@layouts/index';
-import { Switch, Breadcrum, ListBox, InputField } from '@library/core';
+import { Switch, Breadcrum, ListBox, InputField, Text } from '@library/core';
 
 const Home = () => {
   // const [checked, setChecked]: [any, any] = useState(false);
   const [selected, setSelected] = useState({ name: 'Wade Cooper' });
+  const items = [
+    { title: 'Mantine', href: 'https://mantine.dev' },
+    { title: 'Mantine hooks', href: 'https://mantine.dev/hooks/getting-started' },
+    { title: 'use-id', href: 'https://mantine.dev/hooks/use-id' },
+  ].map((item, index) => (
+    <Text variant='link' component='a' href={item.href} key={index}>
+      {item.title}
+    </Text>
+  ));
 
   return (
     <Layout>
@@ -263,45 +272,8 @@ const Home = () => {
       <div className='pb-10 flex flex-col justify-center items-center py-1 border border-blue-600'>
         <u> Breadcrums </u>
         <br />
-        <Breadcrum showHome={true} className={`p-0 bg-white-400`} />
-        <br />
-        <Breadcrum
-          showHome={true}
-          className={`p-0 bg-white-400`}
-          title={'Child Title'}
-          titleLink={'/'}
-        />
-        <br />
-        <Breadcrum
-          showHome={true}
-          className={`p-0 bg-white-400`}
-          title={'Child Title'}
-          titleLink={'/'}
-          parent={[
-            { title: 'parent 1', link: '/' },
-            { title: 'parent 2', link: '/' },
-          ]}
-        />
-        <br />
-        <Breadcrum
-          showHome={true}
-          parent={[
-            { title: 'parent 1', link: '/' },
-            { title: 'parent 2', link: '/' },
-          ]}
-        />
-        <br />
-        <Breadcrum
-          title={'Child Title'}
-          titleLink={'/'}
-          parent={[
-            { title: 'parent 1', link: '/' },
-            { title: 'parent 2', link: '/' },
-          ]}
-        />
-
-        <br />
-        <Breadcrum title={'Child Title'} titleLink={'/'} />
+        <Breadcrum>{items}</Breadcrum>
+        <Breadcrum separator='→'>{items}</Breadcrum>
       </div>
     </Layout>
   );
