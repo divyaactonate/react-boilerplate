00dad79d13106a3b5019786fbadf02c9e06deac5
Kaushal <kaushal.shah@actonate.com>
2021-06-02 19:45:51 +0530

    Bh-7 border fixes

--- src/library/core/FolderCard/FolderCard.styles.ts
+++ src/library/core/FolderCard/FolderCard.styles.ts
@@ -13,27 +13,37 @@ export const getStyles = (props: StylesProps) => {
 
   const css = {
     card: {
-      borderWidth: '1.5px',
+      // borderWidth: '1px',
+      boxShadow: checked
+        ? `0 0 0 1px ${theme.colors.blue[4]}`
+        : `0 0 0 1px ${theme.colors.gray[3]}`,
+
       borderColor: checked ? theme.colors.blue[4] : theme.colors.gray[3],
       '&:hover': {
-        borderColor: theme.colors.blue[4],
+        // borderWidth: '1.5px',
+        // margin: '-1.5px',
+        boxShadow: !checked && `0 0 0 1px ${theme.colors.blue[4]}`,
+        // borderColor: theme.colors.blue[4],
       },
     },
     foldercard: { theme },
   };
 
   const classes = {
-    wrapper: `w-full h-full rounded`,
-    card: cx(`flex flex-col space-y-2 justify-between px-3 py-2 rounded-md`, `overflow-hidden`),
+    wrapper: `w-full box-border h-full rounded`,
+    card: cx(
+      `flex flex-col space-y-2 justify-between pl-2 pr-1 py-2 rounded-md`,
+      `overflow-hidden`
+    ),
     topWrapper: `flex space-x-3 items-center justify-between`,
     topLeftWrapper: `flex items-center truncate space-x-2`,
     topRightWrapper: `flex items-center justify-between space-x-2`,
-    checkBoxClass: `w-3`,
+    checkBoxClass: `w-5`,
     countClass: `text-gray-500 font-normal text-sm leading-tight`,
     titleClass: `text-gray-700 truncate capitalize font-normal text-base leading-tight`,
     bottomWrapper: `flex items-center justify-between flex-row`,
     bottomLeftWrapper: `flex items-center text-xs flex-row space-x-4`,
-    bottomRightWrapper: `flex flex-row space-x-1 text-gray-500`,
+    bottomRightWrapper: `flex flex-row  items-center justify-between space-x-1 text-gray-500`,
     folderIconClass: `w-24 text-blue-500`,
     heartIconClass: `w-4 text-gray-500`,
     tagIconClass: `w-3.5 text-gray-500`,
--- src/library/core/ReportCard/ReportCard.styles.ts
+++ src/library/core/ReportCard/ReportCard.styles.ts
@@ -16,18 +16,26 @@ export const getStyles = (props: StylesProps) => {
     card: {
       borderLeftColor: theme.colors.yellow[8],
       borderLeftWidth: '6px',
-      borderTopWidth: '1.5px',
-      borderBottomWidth: '1.5px',
-      borderRightWidth: '1.5px',
+      // borderTopWidth: '1px',
+      // borderBottomWidth: '1px',
+      // borderRightWidth: '1px',
+      boxShadow: checked
+        ? `0 0 0 1.2px ${theme.colors.blue[4]}`
+        : `0 0 0 1px ${theme.colors.gray[3]}`,
       borderTopColor: checked ? theme.colors.blue[4] : theme.colors.gray[3],
       borderBottomColor: checked ? theme.colors.blue[4] : theme.colors.gray[3],
       borderRightColor: checked ? theme.colors.blue[4] : theme.colors.gray[3],
       color: theme.colorScheme === 'dark' ? theme.white : theme.black,
       backgroundColor: theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.white,
       '&:hover': {
-        borderTopColor: theme.colors.blue[4],
-        borderBottomColor: theme.colors.blue[4],
-        borderRightColor: theme.colors.blue[4],
+        boxShadow: !checked && `0 0 0 1.2px ${theme.colors.blue[4]}`,
+
+        // borderTopWidth: '1.5px',
+        // borderBottomWidth: '1.5px',
+        // borderRightWidth: '1.5px',
+        // borderTopColor: theme.colors.blue[4],
+        // borderBottomColor: theme.colors.blue[4],
+        // borderRightColor: theme.colors.blue[4],
       },
     },
     leftContentText: {
@@ -42,10 +50,10 @@ export const getStyles = (props: StylesProps) => {
     card: cx(`w-full h-full  rounded`, `overflow-hidden shadow `),
     wrapper: cx('flex flex-row'),
     leftWrapper: `w-2/6 border-r border-gray-200`,
-    checkBoxClass: `w-5`,
+    checkBoxClass: `w-8`,
     leftContent: `flex items-center h-full  justify-center`,
     leftContentText: `px-4 break-words text-sm text-center capitalize`,
-    rightWrapper: `w-4/6 flex xs:flex-row flex-col justify-between p-4`,
+    rightWrapper: `w-4/6 flex xs:flex-row flex-col justify-between pl-4 py-4 pr-1`,
     rightTopWrapper: `flex flex-col`,
     title: `text-black truncate capitalize font-semibold text-sm mb-2 leading-none`,
     subTitle: `text-gray-500 font-normal text-xs mb-2 leading-none`,
@@ -53,7 +61,7 @@ export const getStyles = (props: StylesProps) => {
     rightBottompWrapper: `flex items-center justify-between flex-row`,
     rightBottomLeftSection: `flex items-center text-xs flex-row space-x-4`,
     badgeClass: `p-2`,
-    rightBottomRightSection: `flex flex-row space-x-1 text-gray-500`,
+    rightBottomRightSection: `flex flex-row items-center justify-between space-x-1 text-gray-500`,
     iconClass: `w-4 text-gray-500`,
   };
   return { classes, css };
