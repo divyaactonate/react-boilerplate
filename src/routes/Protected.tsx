fcb98b81e8ac03e0d9440935c36d777bf34b4817
Kaushal <kaushal.shah@actonate.com>
2021-06-09 14:01:17 +0530

    refactor: 💡 hooks used , hoc removed

--- src/routes/Protected.tsx
+++ src/routes/Protected.tsx
@@ -1,14 +1,15 @@
-import { inject, observer } from 'mobx-react';
+// import { inject, observer } from 'mobx-react';
 import { Redirect, Route } from 'react-router-dom';
-// import { useAuth } from '@store/index'
-
+import { useAuth } from '@store/index';
 interface Props {
   component: any;
   [name: string]: any;
 }
 
-const ProtectedRoute = ({ component, authStore, ...rest }: Props) => {
-  if (authStore.isAuthenticated === true) {
+const ProtectedRoute = ({ component, ...rest }: Props) => {
+  const { isAuthenticated } = useAuth();
+  // if (authStore.isAuthenticated === true) {
+  if (isAuthenticated === true) {
     // send to route
     return <Route {...rest} component={component} />;
   } else {
@@ -17,4 +18,5 @@ const ProtectedRoute = ({ component, authStore, ...rest }: Props) => {
   }
 };
 
-export default inject(({ store }) => store)(observer(ProtectedRoute));
+// export default inject(({ store }) => store)(observer(ProtectedRoute));
+export default ProtectedRoute;
